615f838ea0da5b7e11fe8c72f784dd88b0d1e736
Quazi fahim <fahimahmad.4388@gmail.com>
2026-01-20 17:12:31 +0530

    admin data manipulation2

--- src/routes/content.route.js
+++ src/routes/content.route.js
@@ -1,11 +1,10 @@
 const express=require('express');
-const { allContent } = require('../controller/content.controller');
 const router=express.Router();
 const {Protect}=require('../middleware/authmiddleware')
 const {CheckAccess}=require('../middleware/access');
-const { createData, update, deleteData } = require('../controller/admin/admin.content.controller');
+const { createData, update, deleteData, getall, } = require('../controller/admin/admin.content.controller');
 
-router.get('/content',Protect,allContent)
+router.get('/content',Protect,getall)
 router.post('/create',Protect,CheckAccess({roles:['admin']}),createData)
 router.put('/:id',Protect,CheckAccess({roles:['admin']}),update)
 router.delete('/:id',Protect,CheckAccess({roles:['admin']}),deleteData)
