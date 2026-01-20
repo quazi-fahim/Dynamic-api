367f8a13645cb678703a1ff03e42c14671949181
Quazi fahim <fahimahmad.4388@gmail.com>
2026-01-20 17:08:18 +0530

    admin data manipulation

--- src/controller/admin/admin.content.controller.js
+++ src/controller/admin/admin.content.controller.js
@@ -16,5 +16,37 @@ exports.getall=async(req,res)=>{
 }
 
 exports.createData=async(req,res)=>{
-    
+    try{
+        const content=await Content.create(req.body)
+     res.status(200).json({content})
+    }catch(err){
+            res.status(500).json({message:"data not found"})
+
+    }
+}
+
+
+//update 
+exports.update=async(req,res)=>{
+
+    try{
+        const content=await Content.findByIdAndUpdate(req,params.id,req.body,{new:true,runValidators:true})
+        res.status(200).json(content)
+    }catch(err){
+            res.status(500).json({message:"data not found"})
+
+    }
+}
+ 
+
+//delete 
+exports.deleteData=async(req,res)=>{
+    try{
+        const content=await Content.findByIdAndDelete(req.params.id,{new:true})
+    res.status(200).json(content)
+    }
+    catch(err){
+            res.status(500).json({message:"data not found"})
+
+    }
 }
--- src/routes/content.route.js
+++ src/routes/content.route.js
@@ -3,7 +3,11 @@ const { allContent } = require('../controller/content.controller');
 const router=express.Router();
 const {Protect}=require('../middleware/authmiddleware')
 const {CheckAccess}=require('../middleware/access');
+const { createData, update, deleteData } = require('../controller/admin/admin.content.controller');
 
 router.get('/content',Protect,allContent)
+router.post('/create',Protect,CheckAccess({roles:['admin']}),createData)
+router.put('/:id',Protect,CheckAccess({roles:['admin']}),update)
+router.delete('/:id',Protect,CheckAccess({roles:['admin']}),deleteData)
 
 module.exports=router
